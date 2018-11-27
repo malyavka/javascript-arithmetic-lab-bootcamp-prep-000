96bf5d477e937404b28feedf65d503e1874f22a1
Maria Obaturova <m.obaturova@gmail.com>
2018-11-27 19:20:31 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+function add(a,b) {
+  result = a + b;
+  return result
+}
+
+function substract(a,b) {
+  result = a - b;
+  return result
+}
+function multiply(a,b) {
+  result = a * b;
+  return result
+}
+function divide(a,b) {
+  result = a / b;
+  return result
+}
+function inc(n) {
+n++;
+return n;
+}
